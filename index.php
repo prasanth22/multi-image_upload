ee41b65bacd5882de2dddb856aa866fe558c8c9c
prasanth22 <madduprasanth1@gmail.com>
2020-02-20 07:21:18 +0530

    first

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP - Multiple Image upload using dropzone.js</title>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>
+</head>
+<body>
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h2>PHP - Multiple Image upload using dropzone.js</h2>
+			<form action="upload.php" enctype="multipart/form-data" class="dropzone">
+				<div>
+					<h3>Upload Multiple Image By Click On Box</h3>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
